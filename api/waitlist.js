1440f918251ce1f0be5a253ed043d4fc0a1c063b
hoard-app <ruipsdurao@hoard-app.com>
2026-02-26 17:11:34 +0000

    Update waitlist.js

--- api/waitlist.js
+++ api/waitlist.js
@@ -28,7 +28,7 @@ function decodeRef(ref) {
   }
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
